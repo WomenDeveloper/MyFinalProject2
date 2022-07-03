6e38bf1ec2d07878b4ae43cdef4d4a009b05de9c
Women Developer <womenndeveloper@gmail.com>
2022-07-03 19:45:04 +0300

    add Autofac and change program.cs configuration

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Autofac;
+using Business.Abstract;
+using Business.Concreate;
+using DataAccess.Abstract;
+using DataAccess.Concreate.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+        }
+        public AutofacBusinessModule()
+        {
+        }
+    }
+}
+
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,16 +1,24 @@
-﻿using Business.Abstract;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Abstract;
 using Business.Concreate;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concreate.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
+    
 
 // Add services to the container.
 //Autofac,Ninjet,CastleWindsor,StructureMap,LightInject,DryInject-->IoC C.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
